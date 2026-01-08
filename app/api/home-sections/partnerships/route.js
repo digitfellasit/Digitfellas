90bd0f7fbe2eb711904d9671640897f3aea95d56
basteen-Dev <basteen@digitfellas.com>
2026-01-08 15:53:32 +0530

    updated

--- app/api/home-sections/partnerships/route.js
+++ app/api/home-sections/partnerships/route.js
@@ -1,6 +1,7 @@
-
 import { NextResponse } from 'next/server'
 import { getPool } from '@/lib/db'
+
+export const dynamic = 'force-dynamic'
 
 export async function GET() {
     const pool = getPool()
